290810b60aae434beb50bd323ed56cd06eb797a8
Aubrie <aubriewoodbine@yahoo.com>
2024-07-04 16:12:25 -0500

    setting the signedin user to the state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+
+  // checks to see if a user is currently signed in and sets them to the state
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }
+    ]
+  }
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

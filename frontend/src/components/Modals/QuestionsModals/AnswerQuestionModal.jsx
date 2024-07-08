b6eaadc92b70d22305e0634fc29775725612bc7a
Aubrie <aubriewoodbine@yahoo.com>
2024-07-08 12:54:03 -0500

    working on answering question modal with add resources functions

--- frontend/src/components/Modals/QuestionsModals/AnswerQuestionModal.jsx
+++ frontend/src/components/Modals/QuestionsModals/AnswerQuestionModal.jsx
@@ -3,9 +3,41 @@ import { useState } from "react";
 
 const AnswerQuestionsModal = ({question}) => {
     const [answer, setAnswer] = useState('');
-    const [resources, setResources] = useState([{id: 1, resource: '', name: '', keywords: []}]);
+    const [resources, setResources] = useState('');
     const [keywords, setKeywords] = useState([]);
     const [hidden, setHidden] = useState(true);
+
+    const handleAddResource = () => {
+        if (resources.length <= 0) {
+            setHidden(false);
+            setResources([{id: 0, url: '', name: '', keywords: []}])
+        } else {
+            const newResource = ([...resources, {id: resources[resources.length - 1].id + 1 , url: '', name: '', keywords: []}])
+            setResources(newResource);
+        }
+    }
+
+    const handleRemoveResource = (id) => {
+        console.log(resources)
+        const removeResource = [...resources]
+        removeResource.splice(id, 1);
+        if (resources.length <= 0) {
+            setHidden(true);
+            setResources('');
+        } else {
+            setResources(removeResource);
+        }
+    }
+
+    const handleSelect = (target) => {
+        const array = [...keywords]
+        array.push(target);
+        setKeywords(array)
+    }
+
+    const handleAnswer = () => {
+        
+    }
 
     return (
         <div>
@@ -13,38 +45,63 @@ const AnswerQuestionsModal = ({question}) => {
             <div>
                 <span>{question.question}</span>
                 <div>
-                    <textarea></textarea>
+                    <textarea
+                        type='text'
+                        value={answer}
+                        onChange={() => setAnswer(e.target.value)}
+                        />
                 </div>
             </div>
             <div>
+                <div>
+                    {resources && resources.reverse().map((resource, idx) => (
+                        <div hidden={hidden} key={idx}>
+                            <div>
+                                <label>Resource URL</label>
+                                <input
+                                    type='URL'
+                                    ></input>
+                            </div>
+                            <div>
+                                <label>Please provide a descriptive name:</label>
+                                <input></input>
+                            </div>
+                            <div>
+                                <label>Keywords</label>
+                                <select onChange={(e) => handleSelect(e.target.value)}>
+                                    <option>Documentation</option>
+                                    <option>React</option>
+                                    <option>Python</option>
+                                    <option>JavaScript</option>
+                                    <option>Thunks</option>
+                                    <option>Redux</option>
+                                    <option>Google</option>
+                                </select>
+                            </div>
+                            <div>
+                                <h3>Keywords</h3>
+                                {keywords.map((ele, idx) => (
+                                    <div key={idx}>
+                                        <span>{ele}</span>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                <button onClick={() => handleRemoveResource(idx)}>Remove Resource</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
                 <span >Did you use any specific resources to help you answer this question?</span>
                 <div>
-                    <button onClick={() => setHidden(false)}>yes</button>
+                    <button onClick={handleAddResource}>Add a Resource</button>
                     <button onClick={() => setHidden(true)}>no</button>
-                </div>
-                <div hidden={hidden}>
-                    <div>
-                        <label>Resource URL</label>
-                        <input
-                            type='URL'
-                            ></input>
-                    </div>
-                    <div>
-                        <label>Please provide a descriptive name:</label>
-                        <input></input>
-                    </div>
-                    <div>
-                        <label>Keywords</label>
-                        <select>
-                            <option>this</option>
-                            <option>that</option>
-                            <option>the other</option>
-                        </select>
-                    </div>
                 </div>
             </div>
             <div>
-                <button>
+                <button
+                    type='submit'
+                    onClick={handleAnswer}>
                     Submit
                 </button>
             </div>

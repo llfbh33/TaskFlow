12f10dd55dd80fd9926791dd6eb6948d6faa3f8f
Aubrie <aubriewoodbine@yahoo.com>
2024-07-26 13:32:29 -0500

    adjusted some styling to make the page of questions more viewable however this did affect the layout of the other pages

--- frontend/src/components/Profile/UsersQuestions/UsersQuestions.jsx
+++ frontend/src/components/Profile/UsersQuestions/UsersQuestions.jsx
@@ -26,21 +26,26 @@ const UsersQuestions = () => {
 
     return (
         <div className='profile-selected-section'>
-            <h1>Questions</h1>
-            <div>
-                <button onClick={askAQuestion} className="add-pointer-cursor">Ask a Qusetion</button>
+            <div className="reflection-title-and-creation">
+                <h1>Questions</h1>
+                <div>
+                    <button onClick={askAQuestion} className="add-pointer-cursor">Ask a Qusetion</button>
+                </div>
             </div>
-            <div>
-                {Object.values(allQuestions).map(ele => (
+            <div className="question-display">
+                {Object.values(allQuestions).map((ele, idx) => (
                     <div key={ele.id} className="questions-card">
-                        <span>{ele.question}</span>
+                        <div className="question-alignment">
+                            <h4>{ele.id} - </h4>
+                            <h4>{ele.question}</h4>
+                        </div>
                         { ele.answer
-                        ? <button onClick={() => viewAnswer === ele ? setViewAnswer('') : setViewAnswer(ele)}>View Answer</button>
-                        : <button onClick={() => handleAnswerQuestion(ele)}>Answer this question</button> }
+                        ? <div className='add-pointer-cursor view-question-answer' onClick={() => viewAnswer === ele ? setViewAnswer('') : setViewAnswer(ele)}>{`View Answer From`}</div>
+                        : <div className='add-pointer-cursor view-question-answer' onClick={() => handleAnswerQuestion(ele)}>Answer this question</div> }
                         {viewAnswer === ele
-                        ? <div>
+                        ? <div className="answer-display">
                             <span>{ele.answer}</span>
-                            <span>{ele.createdAt}</span>
+                            {/* <span>{ele.createdAt}</span> */}
                         </div>
                         : ''}
                     </div>

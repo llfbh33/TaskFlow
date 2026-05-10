0cf825778085201644a0d3bc46bef9c0d9fa6c81
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-10 11:16:32 -0500

    update psql setup for railway

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,13 +1,21 @@
-//pull in sequelize from the db/models folders
 const { sequelize } = require('./db/models');
 
-// have sequelize look at all schemas - if that data does not include the schema in the .env file, create that schema
-sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-    if (!data.includes(process.env.SCHEMA)) {
-        await sequelize.createSchema(process.env.SCHEMA);
-    }
-})
+async function setupSchema() {
+  const schema = process.env.SCHEMA;
 
+  const schemas = await sequelize.showAllSchemas({ logging: false });
 
-// So if there is no schema within sequelize that matches our schema, this code will create one
-// which is why, if you are having migration issues and delete the schema, a new one will be produced when migrating
+  if (!schemas.includes(schema)) {
+    await sequelize.createSchema(schema);
+    console.log(`Created schema: ${schema}`);
+  } else {
+    console.log(`Schema already exists: ${schema}`);
+  }
+
+  await sequelize.close();
+}
+
+setupSchema().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
